fc868b23a88aab6cc381b847db1fa53d89c97f8f
cmcnelis <mcnelis.chris@gmail.com>
2013-11-12 16:24:08 -0600

    Rails Skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Austin42::Application.config.secret_key_base = '210612fd49c0b939af6bc5d03efe9227c5c5d56bc8e01ae6b992686a90d40e1b4d938480d085f099cc675061a58ef6bcf401c32ec42352cd98da0ce9b8e1e714'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Austin42::Application.config.session_store :cookie_store, key: '_austin42_session'
